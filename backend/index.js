4553d16272c81eb0efa27873a34d91b66923487d
Rithas K <rithask@icloud.com>
2024-04-22 03:58:25 +0530

    fix: add 'trust proxy' to get real client IP address

--- backend/index.js
+++ backend/index.js
@@ -27,6 +27,7 @@ app.get('/', (req, res) => {
   res.send('server running successfully')
 })
 
+app.enable('trust proxy');
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server listening on http://localhost:${PORT}`);
